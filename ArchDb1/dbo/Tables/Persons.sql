b0392224ae44c96803e7e5518dc728a6c115ea40
Sairam Kancharla <sairamk2@birlasoft.com>
2022-03-21 10:31:53 +0530

    commiting local changes

---
+++ ArchDb1/dbo/Tables/Persons.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Persons] (
+    [PersonID]  INT           NULL,
+    [LastName]  VARCHAR (255) NULL,
+    [FirstName] VARCHAR (255) NULL,
+    [Address]   VARCHAR (255) NULL,
+    [City]      VARCHAR (255) NULL
+);
+
