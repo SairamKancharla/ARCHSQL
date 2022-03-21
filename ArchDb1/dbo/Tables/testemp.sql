5aa0163cdc8daf11df6369a8c7a8fdb954c758cf
Sairam Kancharla <sairamk2@birlasoft.com>
2022-03-21 12:03:15 +0530

    test commits

---
+++ ArchDb1/dbo/Tables/testemp.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Emplyoees] (
+    [Sno]       INT           NULL,
+    [LastName]  VARCHAR (255) NULL,
+    [FirstName] VARCHAR (255) NULL,
+    [Salary]    INT           NULL
+);
+
