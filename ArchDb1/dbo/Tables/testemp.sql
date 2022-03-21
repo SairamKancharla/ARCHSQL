f1bd37e0bc15d3f895f516daff882f1ed81edf1b
Sairam Kancharla <sairamk2@birlasoft.com>
2022-03-21 12:35:00 +0530

    New column added

--- ArchDb1/dbo/Tables/testemp.sql
+++ ArchDb1/dbo/Tables/testemp.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Emplyoees] (
     [Sno]       INT           NULL,
     [LastName]  VARCHAR (255) NULL,
     [FirstName] VARCHAR (255) NULL,
-    [Salary]    INT           NULL
+    [Salary]    INT           NULL,
+    [rupess]    INT (222) 
 );
 
