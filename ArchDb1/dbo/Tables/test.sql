03e8ce3e91b8e71d39f93b0e6ea5d87390ff3f12
Sairam Kancharla <sairamk2@birlasoft.com>
2022-03-21 13:18:00 +0530

    updating sqlproj file

--- ArchDb1/dbo/Tables/test.sql
+++ ArchDb1/dbo/Tables/test.sql
@@ -1,8 +1,7 @@
-CREATE TABLE [dbo].[Emplyoees] (
+﻿CREATE TABLE [dbo].[test] (
     [Sno]       INT           NULL,
     [LastName]  VARCHAR (255) NULL,
     [FirstName] VARCHAR (255) NULL,
     [Salary]    INT           NULL
-
 );
 
