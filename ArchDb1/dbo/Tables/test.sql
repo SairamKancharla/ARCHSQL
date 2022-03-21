6af22d52b9bd2c7d5d78e2408f8290b75f77eef2
Sairam Kancharla <sairamk2@birlasoft.com>
2022-03-21 14:36:06 +0530

    deleted column

--- ArchDb1/dbo/Tables/test.sql
+++ ArchDb1/dbo/Tables/test.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[test] (
     [Sno]       INT           NULL,
     [LastName]  VARCHAR (255) NULL,
-    [FirstName] VARCHAR (255) NULL,
-    [Company] VARCHAR (255) NULL
+    [FirstName] VARCHAR (255) NULL
+    
    
     
 );
