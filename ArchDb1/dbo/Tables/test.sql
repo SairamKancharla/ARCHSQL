dd0f46ea1b9ee46fc14de248fee7818b2c59bd07
Sairam Kancharla <sairamk2@birlasoft.com>
2022-03-22 13:11:08 +0530

    Updated

--- ArchDb1/dbo/Tables/test.sql
+++ ArchDb1/dbo/Tables/test.sql
@@ -1,11 +1,3 @@
-﻿CREATE TABLE [dbo].[test] (
-    [Sno]       INT           NULL,
-    [LastName]  VARCHAR (255) NULL,
-    [FirstName] VARCHAR (255) NULL
-   
-   
-    
-);
-
+﻿
 ALTER TABLE [dbo].[test]
 Add [Mobile Number] INT;
